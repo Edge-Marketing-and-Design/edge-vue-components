1a209359aa92ef82392c25b596148fa1f524efb9
sethjfischer <seth@itsgeek2me.net>
2024-11-14 16:33:55 -0600

    updates

--- components/editor.vue
+++ components/editor.vue
@@ -30,6 +30,14 @@ const props = defineProps({
     type: String,
     default: '',
   },
+  stringsToUpperCase: {
+    type: Boolean,
+    default: false,
+  },
+  saveRedirectOverride: {
+    type: String,
+    default: '',
+  },
 })
 
 const newDoc = computed(() => {
@@ -134,9 +142,19 @@ const title = computed(() => {
 
 const onSubmit = async () => {
   state.bypassUnsavedChanges = true
+  Object.keys(state.workingDoc).forEach((key) => {
+    if (typeof state.workingDoc[key] === 'string' && props.stringsToUpperCase) {
+      state.workingDoc[key] = state.workingDoc[key].toUpperCase()
+    }
+  })
   edgeFirebase.storeDoc(`${edgeGlobal.edgeState.organizationDocPath}/${props.collection}`, state.workingDoc)
   edgeGlobal.edgeState.changeTracker = {}
-  router.push(`/app/dashboard/${props.collection}`)
+  if (props.saveRedirectOverride) {
+    router.push(props.saveRedirectOverride)
+  }
+  else {
+    router.push(`/app/dashboard/${props.collection}`)
+  }
 }
 
 onBeforeMount(async () => {
