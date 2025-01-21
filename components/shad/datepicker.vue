5f0ac53f83f51eb917e5fc8f17ece410a0d525ab
sethjfischer <seth@itsgeek2me.net>
2025-01-21 11:56:09 -0500

    updates

--- components/shad/datepicker.vue
+++ components/shad/datepicker.vue
@@ -1,8 +1,8 @@
 <script setup>
 import { useVModel } from '@vueuse/core'
-import { CalendarDate, getLocalTimeZone, parseDate, today } from '@internationalized/date'
+import { CalendarDate, parseDate } from '@internationalized/date'
 import { Calendar as CalendarIcon } from 'lucide-vue-next'
-import { ref } from 'vue'
+import { onMounted, ref, watch } from 'vue'
 import { useField } from 'vee-validate'
 import {
   FormControl,
@@ -16,6 +16,7 @@ import { Button } from '@/components/ui/button'
 import { cn } from '@/lib/utils'
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
 import { Calendar } from '@/components/ui/calendar'
+
 const props = defineProps({
   label: {
     type: String,
@@ -30,12 +31,36 @@ const props = defineProps({
     required: true,
   },
   modelValue: {
-    type: String,
+    type: [String, Array],
     required: false,
     default: undefined,
   },
+  multiple: {
+    type: Boolean,
+    required: false,
+    default: false,
+  },
+  minValue: {
+    type: String,
+    required: false,
+    default: '1900-01-01',
+  },
+  maxValue: {
+    type: String,
+    required: false,
+    default: '2099-01-14',
+  },
 })
+
 const emits = defineEmits(['update:modelValue'])
+
+const getCalendarDate = (dateString) => {
+  if (!dateString)
+    return null // Handle cases where the date string is not provided
+  const [year, month, day] = dateString.split('-').map(Number)
+  return new CalendarDate(year, month, day)
+}
+
 const modelValue = useVModel(props, 'modelValue', emits, {
   passive: false,
   prop: 'modelValue',
@@ -43,28 +68,60 @@ const modelValue = useVModel(props, 'modelValue', emits, {
 
 const { setValue } = useField(props.name)
 
-const value = ref()
+const value = ref(props.multiple ? [] : null)
 
-const formatDate = (date) => {
-  const formatter = new Intl.DateTimeFormat('en-CA', {
-    timeZone: 'UTC', // Explicitly set to UTC
-    year: 'numeric',
-    month: '2-digit',
-    day: '2-digit',
-  })
-  return formatter.format(new Date(date)).replace(/-/g, '-') // Output: YYYY-MM-DD
+const formatDate = (dates) => {
+  const pad = num => String(num).padStart(2, '0')
+
+  if (Array.isArray(dates)) {
+    return dates
+      .map(date => `${date.year}-${pad(date.month)}-${pad(date.day)}`)
+      .join(', ')
+  }
+  else if (dates) {
+    return `${dates.year}-${pad(dates.month)}-${pad(dates.day)}`
+  }
+  return ''
 }
-watch(() => value.value, (value) => {
-  modelValue.value = formatDate (value)
-  setValue(`${value.year}-${value.month}-${value.day}`)
+
+watch(() => value.value, (newValue) => {
+  if (props.multiple && Array.isArray(newValue)) {
+    // Sort dates in ascending order
+    newValue.sort((a, b) => {
+      const dateA = new Date(a.year, a.month - 1, a.day)
+      const dateB = new Date(b.year, b.month - 1, b.day)
+      return dateA - dateB
+    })
+  }
+
+  modelValue.value = props.multiple
+    ? newValue.map(date => formatDate(date))
+    : formatDate(newValue)
+  setValue(
+    props.multiple
+      ? newValue.map(date => `${date.year}-${date.month}-${date.day}`).join(', ')
+      : `${newValue?.year}-${newValue?.month}-${newValue?.day}`,
+  )
 })
 
 const placeholder = ref()
 
 onMounted(() => {
-  console.log('modelValue', props.modelValue)
-  if (props.modelValue) {
-    value.value = props.modelValue ? parseDate(props.modelValue) : undefined
+  if (props.multiple) {
+    if (Array.isArray(props.modelValue) && props.modelValue.length > 0) {
+      value.value = props.modelValue.map(date => parseDate(date))
+    }
+    else {
+      value.value = [] // Set to an empty array if modelValue is empty or invalid
+    }
+  }
+  else {
+    if (typeof props.modelValue === 'string' && props.modelValue.trim() !== '') {
+      value.value = parseDate(props.modelValue)
+    }
+    else {
+      value.value = null // Set to null if modelValue is empty or invalid
+    }
   }
 })
 </script>
@@ -77,12 +134,21 @@ onMounted(() => {
         <PopoverTrigger as-child>
           <FormControl>
             <Button
-              variant="outline" :class="cn(
-                'w-[240px] ps-3 text-start font-normal',
-                !value && 'text-muted-foreground',
+              variant="outline"
+              :class="cn(
+                'w-full ps-3 text-start font-normal',
+                (!value || (Array.isArray(value) && value.length === 0)) && 'text-muted-foreground',
               )"
             >
-              <span>{{ value ? value : "Pick a date" }}</span>
+              <template v-if="Array.isArray(value)">
+                <edge-chip v-for="date in value" :key="date" class="mr-1">
+                  {{ date }}
+                </edge-chip>
+              </template>
+              <span v-else-if="value">
+                {{ value }}
+              </span>
+
               <CalendarIcon class="ms-auto h-4 w-4 opacity-50" />
             </Button>
             <input hidden>
@@ -92,10 +158,11 @@ onMounted(() => {
           <Calendar
             v-model:placeholder="placeholder"
             v-model="value"
+            :multiple="props.multiple"
             :calendar-label="props.label"
             initial-focus
-            :min-value="new CalendarDate(1900, 1, 1)"
-            :max-value="today(getLocalTimeZone())"
+            :min-value="getCalendarDate(props.minValue)"
+            :max-value="getCalendarDate(props.maxValue)"
           />
         </PopoverContent>
       </Popover>
