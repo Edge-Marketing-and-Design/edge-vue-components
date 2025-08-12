80d09198d77c4ec3bee51afdf225f14f01653958
sethjfischer <seth@itsgeek2me.net>
2025-08-12 13:53:46 -0600

    updates

--- components/shad/button.vue
+++ components/shad/button.vue
@@ -9,12 +9,28 @@ const props = defineProps({
     type: String,
     required: false,
   },
+  target: {
+    type: String,
+    required: false,
+  },
 })
 const router = useRouter()
+function handleNavigation() {
+  if (props.target === '_blank') {
+    window.open(props.to, '_blank')
+  }
+  else {
+    router.push(props.to)
+  }
+}
+</script>
+
+<script setup>
+
 </script>
 
 <template>
-  <Button v-if="props.to" :type="props.type" @click="router.push(props.to)">
+  <Button v-if="props.to" :type="props.type" @click.stop.prevent="handleNavigation">
     <slot />
   </Button>
   <Button v-else :type="props.type">
