5dd34084f057114ef3da5e933c6d73c469d28d08
sethjfischer <seth@itsgeek2me.net>
2026-06-15 00:12:34 -0400

    Reduce startup lead snapshot load

--- components/auth.vue
+++ components/auth.vue
@@ -58,27 +58,65 @@ const effectiveRegistrationCode = computed(() =>
 )
 // const edgeGlobal = inject('edgeGlobal')
 
+let activeLoginKey = ''
+let completedLoginKey = ''
+let activeLoginPromise = null
+
+const getLoginKey = () => {
+  const rolesKey = (edgeFirebase.user?.roles || [])
+    .map(role => `${role.collectionPath || ''}:${role.role || ''}`)
+    .join('|')
+  return [
+    edgeFirebase.user?.loggedIn === true ? '1' : '0',
+    edgeFirebase.user?.uid || '',
+    rolesKey,
+    edgeGlobal.edgeState.currentOrganization || '',
+  ].join('::')
+}
+
 const doLogin = async () => {
-  edgeGlobal.edgeState.user = edgeFirebase.user
-  emit('update:auth', edgeFirebase.user)
-  if (edgeFirebase.user.loggedIn) {
-    await edgeGlobal.getOrganizations(edgeFirebase)
-    const storedOrganization = localStorage.getItem('organizationID')
-    if (storedOrganization && edgeGlobal.edgeState.organizations.some(org => org.docId === storedOrganization)) {
-      await edgeGlobal.setOrganization(storedOrganization, edgeFirebase)
+  const loginKey = getLoginKey()
+  if (completedLoginKey === loginKey)
+    return
+  if (activeLoginPromise && activeLoginKey === loginKey)
+    return activeLoginPromise
+
+  activeLoginKey = loginKey
+  activeLoginPromise = (async () => {
+    edgeGlobal.edgeState.user = edgeFirebase.user
+    emit('update:auth', edgeFirebase.user)
+    if (edgeFirebase.user.loggedIn) {
+      await edgeGlobal.getOrganizations(edgeFirebase)
+      const storedOrganization = localStorage.getItem('organizationID')
+      if (storedOrganization && edgeGlobal.edgeState.organizations.some(org => org.docId === storedOrganization)) {
+        await edgeGlobal.setOrganization(storedOrganization, edgeFirebase)
+      }
+      else if (edgeGlobal.edgeState.currentOrganization) {
+        await edgeGlobal.setOrganization(edgeGlobal.edgeState.currentOrganization, edgeFirebase)
+      }
+      else if (edgeGlobal.edgeState.organizations.length > 0) {
+        await edgeGlobal.setOrganization(edgeGlobal.edgeState.organizations[0].docId, edgeFirebase)
+      }
     }
-    else if (edgeGlobal.edgeState.currentOrganization) {
-      await edgeGlobal.setOrganization(edgeGlobal.edgeState.currentOrganization, edgeFirebase)
-    }
-    else if (edgeGlobal.edgeState.organizations.length > 0) {
-      await edgeGlobal.setOrganization(edgeGlobal.edgeState.organizations[0].docId, edgeFirebase)
-    }
+    completedLoginKey = loginKey
+  })()
+
+  try {
+    await activeLoginPromise
+  }
+  finally {
+    if (activeLoginKey === loginKey)
+      activeLoginPromise = null
   }
 }
 onBeforeMount(() => {
   doLogin()
 })
-watch(edgeFirebase.user, async () => {
+watch(() => [
+  edgeFirebase.user?.loggedIn,
+  edgeFirebase.user?.uid,
+  edgeFirebase.user?.roles?.length,
+], async () => {
   doLogin()
 })
 </script>
